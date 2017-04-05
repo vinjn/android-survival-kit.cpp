46c7bd77cdd96d0c753014082d11455c9ad1fd87
vinjn <vinjn.z@gmail.com>
2017-04-05 14:39:26 +0800

    re-org (cont)

--- _template/jni/main.cpp
+++ _template/jni/main.cpp
@@ -24,7 +24,7 @@
 
 #include <android/sensor.h>
 #include <android/log.h>
-#include "../native_app_glue/android_native_app_glue.h"
+#include "../../native_app_glue/android_native_app_glue.h"
 
 #define LOGI(...) ((void)__android_log_print(ANDROID_LOG_INFO, "native-activity", __VA_ARGS__))
 #define LOGW(...) ((void)__android_log_print(ANDROID_LOG_WARN, "native-activity", __VA_ARGS__))
