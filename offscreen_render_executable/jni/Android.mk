dd756d74caf129d603746508ce7aa4e28504e4ef
vinjn <vinjn.z@gmail.com>
2017-05-16 16:43:48 +0800

    Add offscreen_render_executable

---
+++ offscreen_render_executable/jni/Android.mk
@@ -0,0 +1,12 @@
+# Android.mk
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../native_app_glue
+
+LOCAL_MODULE := offscreen_render_executable
+LOCAL_SRC_FILES := main.cpp
+LOCAL_SRC_FILES += ../../native_app_glue/android_native_app_glue.c
+
+include $(BUILD_EXECUTABLE)
---
+++ offscreen_render_executable/jni/Application.mk
@@ -0,0 +1,3 @@
+APP_ABI := armeabi-v7a arm64-v8a
+# APP_MODULES := libEGL libGLESv1_CM libGLESv3
+# APP_STL := stlport_static
